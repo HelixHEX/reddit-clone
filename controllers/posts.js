61c1b71a8984c6eddfc2ee0d51c22e530d94d95a
Elias <e.wambugu192@gmail.com>
2023-05-14 22:02:56 -0700

    Users can see individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,21 +4,32 @@ import Post from "../models/post.js";
 const router = express.Router();
 
 router.post("/new", async (req, res) => {
-  const post = await new Post(req.body);
-  
-  await post.save().then(() => {
-    res.redirect('/');
-  })
+  try {
+    const post = await new Post(req.body);
+
+    await post.save().then(() => {
+      res.redirect("/");
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
 });
 
 router.get("/new", (_req, res) => res.render("posts-new"));
 
-router.get('/id/:id', (req, res) => {
-  Post.findById(req.params.id).then((post) => {
-    res.render('posts-show', {post})
-  }).catch((err) => {
-    console.log(err.message)
-  })
-})
+router.get("/:id", async (req, res) => {
+  try {
+    await Post.findById(req.params.id)
+      .lean()
+      .then((post) => {
+        res.render("posts-show", { post });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  } catch (e) {
+    console.log(e.message);
+  }
+});
 
 export default router;
