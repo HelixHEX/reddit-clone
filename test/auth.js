60e952da45ee72f9c4475eb69b3c77ecb88d6e63
Elias <e.wambugu192@gmail.com>
2023-05-15 01:13:28 -0700

    Implemented remaining authentication tests

--- test/auth.js
+++ test/auth.js
@@ -13,27 +13,47 @@ const agent = chai.request.agent(app);
 
 const User = require("../models/user");
 
-describe("User", function () {
-  it("should not be able to signin if they have not registered", function (done) {
+describe("User", () => {
+  it("should not be able to signin if they have not registered", (done) => {
     agent
       .post("/signin", { email: "wrong@example.com", password: "nope" })
-      .end(function (err, res) {
+      .end((err, res) => {
         res.should.have.status(401);
         done();
       });
   });
 
-  it('should be able to signup', function (done) {
-    User.findOneAndRemove({ username: 'testone' }, function() {
+  it("should be able to signup", (done) => {
+    User.findOneAndRemove({ username: "testone" }, () => {
       agent
-        .post('/signup')
-        .send({ username: 'testone', password: 'password' })
-        .end(function (err, res) {
+        .post("/signup")
+        .send({ username: "testone", password: "password" })
+        .end((err, res) => {
           console.log(res.body);
           res.should.have.status(200);
-          agent.should.have.cookie('nToken');
+          agent.should.have.cookie("nToken");
           done();
         });
     });
+
+    // logout
+    it("should be able to logout", (done) => {
+      agent.get("/logout").end((err, res) => {
+        res.should.have.status(200);
+        agent.should.not.have.cookie("nToken");
+        done();
+      });
+    });
+  });
+
+  it("should be able to login", (done) => {
+    agent
+      .post("/login")
+      .send({ username: "testone", password: "password" })
+      .end((err, res) => {
+        res.should.have.status(200);
+        agent.should.have.cookie("nToken");
+        done();
+      });
   });
 });
